d8568ea32d64401166028636795dfcd3182f8e8c
Felix Silberstein <felixsilberstein@Felixs-MacBook-Air.local>
2019-01-22 09:36:02 +1100

    Made AllInfo Work, some changes to the Azimuth function to enable faster calculation.

--- simplesolar.py
+++ simplesolar.py
@@ -41,47 +41,66 @@ class Position(object):
 
         return(UTS_datetime + pd.Timedelta(minutes = 4*(self.longitude)+E))
     
-    def hour_angle(self, UTS_datetime):
+    def hour_angle(self, UTS_datetime = None, solarTime = None):
         """Takes a pd.datetime value for solar time and returns the solar hour angle to the second."""
+        try:
+            decimal_hour_time = solarTime.hour + solarTime.minute/60 +solarTime.second/3600
+            
+        except:
+            solar_time = self.solar_time(UTS_datetime)
+            decimal_hour_time = solar_time.hour+solar_time.minute/60+solar_time.second/3600
+            
+        return(360*(decimal_hour_time-12)/24)
 
-        solar_time = self.solar_time(UTS_datetime)
-        decimal_hour_time = solar_time.hour+solar_time.minute/60+solar_time.second/3600
-
-        return(360*(decimal_hour_time-12)/24)                      
-
-    def sun_rise(self, UTS_datetime):
+    def sun_rise(self, UTS_datetime = None, declinationAngle = None):
         """Returns the hour angle at sunrise/sunset."""
-
-        delta = math.radians(declination_angle(UTS_datetime))
-        phi = math.radians(self.latitude)
+        try:
+            delta = math.radians(declinationAngle)
+            phi = math.radians(self.latitude)
+            
+        
+        except:
+            delta = math.radians(declination_angle(UTS_datetime))
+            phi = math.radians(self.latitude)
 
         return (math.degrees(math.acos(-math.tan(phi)*math.tan(delta))))                                     
                                      
                                      
                                      
     #Methods for Position:
-                                     
-    def zenith_angle(self, UTS_datetime):
-        """Returns the angle between the sun and a vertical line at that location. The angle of incidence between the sun and a flat plate collector. Eq. 1.6.5 Duffie and Beckman."""
-    
-        delta = math.radians(declination_angle(UTS_datetime))
-        phi = math.radians(self.latitude)
-        omega = math.radians(self.hour_angle(UTS_datetime))
-    
-    
-        a = math.cos(phi)*math.cos(delta)*math.cos(omega)+math.sin(phi)*math.sin(delta)
 
-        if abs(math.degrees(math.acos(a))) > 90:
+    def zenith_angle(self, UTS_datetime = None, declinationAngle = None, hourAngle = None):
+        """Determines the zenith angle based on inputs. """
+
+        phi = math.radians(self.latitude)
+        try:
+            delta = math.radians(declinationAngle)
+        except:
+            delta = math.radians(declination_angle(UTS_datetime))
+        try:
+            omega = math.radians(hourAngle)
+        except:
+            omega = math.radians(self.hour_angle(UTS_datetime)) 
+          
+        x = math.cos(phi)*math.cos(delta)*math.cos(omega)+math.sin(phi)*math.sin(delta)
+
+        if abs(math.degrees(math.acos(x))) > 90:
             return(None)
         else:
-            return (math.degrees(math.acos(a)))
+            return (math.degrees(math.acos(x)))
         
-    def elevation_angle(self, UTS_datetime):
+    def elevation_angle(self, UTS_datetime = None, declinationAngle = None, hourAngle = None):
         """Determines the elevation angle based on inputs. """
 
-        delta = math.radians(declination_angle(UTS_datetime))
         phi = math.radians(self.latitude)
-        omega = math.radians(self.hour_angle(UTS_datetime))   
+        try:
+            delta = math.radians(declinationAngle)
+        except:
+            delta = math.radians(declination_angle(UTS_datetime))
+        try:
+            omega = math.radians(hourAngle)
+        except:
+            omega = math.radians(self.hour_angle(UTS_datetime)) 
 
         x = np.arcsin(np.cos(delta)*np.cos(phi)*np.cos(omega)+np.sin(delta)*np.sin(phi))
 
@@ -91,42 +110,57 @@ class Position(object):
         else:
             return (None)        
         
-    def solar_azimuth(self, UTS_datetime):
+    def solar_azimuth(self, UTS_datetime = None, declinationAngle = None, hourAngle = None, zenithAngle = 'Not Provided'):
 
         """The solar azimuth. Eq 1.6.6 Duffie and Beckman."""
-    
-        delta = math.radians(declination_angle(UTS_datetime))
         phi = math.radians(self.latitude)
-        omega = math.radians(self.hour_angle(UTS_datetime)) 
-
-        if self.zenith_angle(UTS_datetime) is None:
-            return None
-    
-        else:
-            theta_z = np.radians(self.zenith_angle(UTS_datetime))
-                                     
-            a = (np.cos(theta_z)*np.sin(phi)-np.sin(delta))/(np.sin(theta_z)*np.cos(phi))
-
-            if abs(a)>1:
-                a=np.sign(a)*1
-            x = np.degrees(np.sign(omega)*abs(np.arccos(a)))
+        try:
+            delta = math.radians(declinationAngle)
+        except:
+            delta = math.radians(declination_angle(UTS_datetime))
+        try:
+            omega = math.radians(hourAngle)
+        except:
+            omega = math.radians(self.hour_angle(UTS_datetime))
+            
+        try:
+            theta_z = np.radians(zenithAngle)
+            
+        except:
+            try:
+                theta_z = np.radians(self.zenith_angle(UTS_datetime))
+            except:
+                return None
+            
+        a = (np.cos(theta_z)*np.sin(phi)-np.sin(delta))/(np.sin(theta_z)*np.cos(phi))
         
-            if phi >= 0:
-                return(x)
-            elif x>0:
-                return 180-x
-            elif x<0:
-                return (-x-180)
-            else:
-                return 0
+        if abs(a)>1:
+            a=np.sign(a)*1
+            
+        x = np.degrees(np.sign(omega)*abs(np.arccos(a)))
+        
+        if phi >= 0:
+            return(x)
+        elif x>0:
+            return 180-x
+        elif x<0:
+            return (-x-180)
+        else:
+            return 0
 
-    def solar_azimuth_topocentric(self, UTS_datetime):
+    def solar_azimuth_topocentric(self, UTS_datetime = None, declinationAngle = None, hourAngle = None):
         """Returns the solar azimuth at a time/latitude. Eq. 7 page section 12.6 from 'Fundimentals of Renewable
                 Energy Processes' by Aldo Vieira da Rosa."""
 
-        delta = math.radians(declination_angle(UTS_datetime))
         phi = math.radians(self.latitude)
-        omega = math.radians(self.hour_angle(UTS_datetime))    
+        try:
+            delta = math.radians(declinationAngle)
+        except:
+            delta = math.radians(declination_angle(UTS_datetime))
+        try:
+            omega = math.radians(hourAngle)
+        except:
+            omega = math.radians(self.hour_angle(UTS_datetime))
 
         x = np.sin(omega)/(np.sin(phi)*np.cos(omega)-np.cos(phi)*np.tan(delta))
 
@@ -210,15 +244,25 @@ class Position(object):
         """Returns a dataframe with specified information for the current year for timestamps at 'frequency' at Position self. Frequency as per Pandas date_range eg 'H' for hour, '10min' for ten minute) see https://pandas.pydata.org/pandas-docs/stable/timeseries.html#timeseries-offset-aliases"""
         
         date_rng = pd.date_range(start='1/1/%d' %pd.Timestamp.utcnow().year, end='1/1/%d' %(pd.Timestamp.utcnow().year+1), freq= frequency, closed = 'left')
-        df = pd.DataFrame(date_rng, columns=['date'])
-        df['SolarTime'] = df.apply(lambda x: self.solar_time(x['date']), axis = 1)
-        df['HourAngle'] = df.apply(lambda x: self.hour_angle(x['date']), axis = 1)
-        df['Zenith'] = df.apply(lambda x: self.zenith_angle(x['date']), axis = 1)
-        df['Elevation'] = df.apply(lambda x: self.elevation_angle(x['date']), axis = 1)
-        df['Azimuth'] = df.apply(lambda x: self.solar_azimuth(x['date']), axis = 1)
-        df['Topocentric_Azimuth'] = df.apply(lambda x: self.solar_azimuth_topocentric(x['date']), axis = 1)
-        return df
         
+        df = pd.DataFrame(date_rng, columns=['date'])
+        
+        df['SolarTime'] = df.apply(lambda x: self.solar_time(x['date']), axis = 1)
+        
+        df['HourAngle'] = df.apply(lambda x: self.hour_angle(x['date'], x['SolarTime']), axis = 1)
+
+        df['DeclinationAngle'] = df.date.apply(declination_angle)
+        
+        df['Zenith'] = df.apply(lambda x: self.zenith_angle(x['date'], x['DeclinationAngle'], x['HourAngle']), axis = 1)
+        
+        df['Elevation'] = df.apply(lambda x: self.elevation_angle(x['date'], x['DeclinationAngle'], x['HourAngle']), axis = 1)
+        
+        df['Azimuth'] = df.apply(lambda x: self.solar_azimuth(x['date'], x['DeclinationAngle'], x['HourAngle'], x['Zenith']), axis = 1)
+        
+        df['Topocentric_Azimuth'] = df.apply(lambda x: self.solar_azimuth_topocentric(x['date'], x['DeclinationAngle'], x['HourAngle']), axis = 1)
+        
+        return df
+
 class SolarOrientation(object):
     """Orientation and slope of the site."""
 
