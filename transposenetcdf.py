f80fc64488c1eb89f341942b03769b86bdb26927
Felix Silberstein <felixsilberstein@Felixs-MacBook-Air.local>
2019-02-26 10:00:15 +1100

    Added a function to concatenate year by year

--- transposenetcdf.py
+++ transposenetcdf.py
@@ -28,3 +28,16 @@ def convert_time_to_double(path = '/data/marble/sandbox/jsilberstein/', target =
 	for file in progressbar.progressbar(get_files(path)):
 		nco.ncap2(options= ['-O','-s','time=time.double()'], input =  file[0], output = '{}dbl_tm_{}'.format(target,file[1])) 
 
+def concat_files(path = '/data/marble/sandbox/jsilberstein/timedouble/', target = '/data/marble/sandbox/jsilberstein/timedoubleyears/'):
+	year_lists = []
+	year = []
+	files = get_files(path)
+	for x in files:
+		year.append(x[0])
+		if x[1][-5:-3] == 12:
+			year_lists.append(year)
+			year = []
+	for x in progressbar.progressbar(year_lists):
+		nco.ncrcat(input = x, output = '{}radiation_{}.nc'.format(target, x[1][-9:-5]))
+
+	return year_lists 
