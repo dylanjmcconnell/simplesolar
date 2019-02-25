3b9eaf42c507f93b27072e1f913df58dfdd6e3c0
Felix Silberstein <felixsilberstein@Felixs-MacBook-Air.local>
2019-02-26 10:52:52 +1100

    test

--- transposenetcdf.py
+++ transposenetcdf.py
@@ -10,9 +10,9 @@ def get_files(mypath):
     """Returns a list of files that have filenames starting with "firstnine" as list of lists [fullpath, filename]."""
     fileinfo = []
     for root, dirs, files in os.walk(mypath, topdown = False):
-        for name in files:
-            if name[-2:] == 'nc':
-                fileinfo.append([os.path.join(root,name),name])
+    	for name in files:
+    		if name[-2:] == 'nc':
+    			fileinfo.append([os.path.join(root,name), name])
     return (fileinfo)
 
 
@@ -32,12 +32,13 @@ def concat_files(path = '/data/marble/sandbox/jsilberstein/timedouble/', target 
 	year_lists = []
 	year = []
 	files = get_files(path)
+	print (files)
 	for x in files:
 		year.append(x[0])
 		if x[1][-5:-3] == 12:
 			year_lists.append(year)
 			year = []
-	for x in progressbar.progressbar(year_lists):
-		nco.ncrcat(input = x, output = '{}radiation_{}.nc'.format(target, x[1][-9:-5]))
+	# for x in progressbar.progressbar(year_lists):
+	# 	nco.ncrcat(input = x, output = '{}radiation_{}.nc'.format(target, x[1][-9:-5]))
 
 	return year_lists 
